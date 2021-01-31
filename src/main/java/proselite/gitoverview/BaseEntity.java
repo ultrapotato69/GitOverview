7581574c25a961936df01f6680be07751cbf90c7
ultrapotato69 <ultrapotato69@gmail.com>
2021-01-31 05:36:03 +0300

    java

---
+++ src/main/java/proselite/gitoverview/BaseEntity.java
@@ -0,0 +1,25 @@
+package proselite.gitoverview;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    public boolean isNew() {
+        return id == null;
+    }
+}
